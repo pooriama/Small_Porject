ede020f402f2f4175d69db3acbca809733c36dfe
Pooria <pooria.mazaheri@gmail.com>
2024-03-28 17:23:00 -0400

    add new codes.

--- sum_root_to_leaf.py
+++ sum_root_to_leaf.py
@@ -4,3 +4,13 @@ def sum_root_to_leaf(tree, partial_path_sum=0):
 
     partial_path_sum = partial_path_sum * 2 + tree.data
     return sum_root_to_leaf(tree.left, partial_path_sum) + sum_root_to_leaf(tree.right, partial_path_sum)
+
+
+def sum_root_to_leafs(tree, sum=0):
+    if not tree:
+        return 0
+
+    sum = sum * 2 + tree.data
+    if not tree.left and not tree.right:
+        return sum
+    return sum_root_to_leafs(tree.left, sum) + sum_root_to_leafs(tree.right, sum)
